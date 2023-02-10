72b4a4ba0a35d4e230911b3844f94455de2c5a40
qiuxu <qiuxu@pinduoduo.com>
2023-02-10 14:33:22 +0800

    fix crash

--- utils/src/main/cpp/test/test_jni_helper.h
+++ utils/src/main/cpp/test/test_jni_helper.h
@@ -8,7 +8,7 @@
 
 inline void test_jni_helper(JNIEnv* env) {
     jclass cls = android::FindClassOrDie(env, "com/wyc/utils/UtilsMainActivity");
-    jfieldID  generic_test = android::GetFieldIDOrDie(env, cls, "generic_test", "Landroid/widget/Button;");
-    jmethodID onCreate = android::GetMethodIDOrDie(env, cls, "onCreate", "(Landroid/os/Bundle;)V");
-    jclass clsRef = android::MakeGlobalRefOrDie(env, cls);
+//    jfieldID  generic_test = android::GetFieldIDOrDie(env, cls, "generic_test", "Landroid/widget/Button;");
+//    jmethodID onCreate = android::GetMethodIDOrDie(env, cls, "onCreate", "(Landroid/os/Bundle;)V");
+//    jclass clsRef = android::MakeGlobalRefOrDie(env, cls);
 }
